3d3fb0df6cf6a76e751fc73ace71a9f240eb05c5
Joel <joelwilsonmt@gmail.com>
2019-03-02 16:45:33 -0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import RouteTesting from './components/RouteTesting/RouteTesting';
 
 class App extends Component {
 	constructor(props){
@@ -9,12 +9,13 @@ class App extends Component {
 			message: "this would be your account number but no web3 yet"
 		}
 	}
-	
+
   render() {
     return (
       <div className="App">
         <p>This is a test!</p>
-				<p>{this.state.message}</p>			
+				<p>{this.state.message}</p>
+				<RouteTesting />
       </div>
     );
   }
