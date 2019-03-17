1f702d49b783b47a903e17d7fe3010e6ca134b6d
Joel <43355957+joelwilsonmt@users.noreply.github.com>
2019-03-17 16:08:10 -0600

    Revert "Stitch"

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Typography from '@material-ui/core/Typography';
@@ -8,10 +7,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 
 
 const Loading = (props) => {
+  const [open, setOpen] = React.useState(true);
+  const handleClose = () => {
+    setOpen(false);
+  }
   return <Dialog
-    open={true}
+    open={open}
     disableBackdropClick={true}
-    disableEscapeKeyDown={true}>
+    disableEscapeKeyDown={false}
+    onClose={handleClose}>
     <DialogContent style={{"text-align": "center"}}>
       <DialogTitle id="simple-dialog-title">Loading...</DialogTitle>
       <img alt="loading" width={100} src="https://media.giphy.com/media/dnoa8RlmGDXpDuj1D9/giphy.gif"/><br/>
