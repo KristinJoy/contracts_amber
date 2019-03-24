fa8799f478001bb3cc967ae9263714ee10c27ff1
Jamie Iguchi <jamieiguchi@Jamies-MacBook-Pro.local>
2019-03-24 10:16:01 -0600

    Added spacing between cards on CreateNewContract

--- src/components/CreateNewContract.js
+++ src/components/CreateNewContract.js
@@ -8,6 +8,7 @@ import SideBar from "./SideBar.js";
 import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 
 
 const styles = theme => ({
@@ -25,54 +26,66 @@ const styles = theme => ({
   }
 });
 
+
 class NewContractCards extends React.Component {
 
   render() {
     const { classes } = this.props;
 
     return (
+
       <SideBar>
+
         <div className={classes.root}>
-        
-        <Typography variant="h4">Create New Contract</Typography>
-        
-        <Card className={classes.card}>
-          <CardContent>
-              <Typography className={classes.heading}>
-                Service Contract
-              </Typography>
-              <Typography>
-                This contract allows you enter into a service agreement with
-                another party. The person performing the service creates the
-                contract. Once it’s deployed, the person receiving the service will
-                add funds to the contract. Once the work is completed, both
-                parties will finalize the contract, via your dashboards and funds
-                will be released.
-              </Typography>
-            <Button variant="contained" color="primary" className={classes.button} component={Link} to='/deploy/service_agreement'>
-              Create This Contract
-            </Button>
-          </CardContent>
-        </Card>
 
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography className={classes.heading}>
-                Rainy Day Contract
-              </Typography>
-              <Typography className={classes.secondaryHeading} />
-              <Typography>
-                This contract allows you to hold money for a rainy day in which
-                ever city you specify. Once the weather report shows that it’s
-                raining, the money is transferred into your account and you are
-                free to enjoy your rainy day.
-              </Typography>
-              <Button variant="contained" color="primary" className={classes.button} component={Link} to='/deploywithvalue/rainy_day'>
+        <Grid container spacing={24}>
+
+          <Grid item xs={12}>
+            <Typography variant="h4">Create New Contract</Typography>
+          </Grid>
+            
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardContent>
+                  <Typography className={classes.heading}>
+                    Service Contract
+                  </Typography>
+                  <Typography>
+                    This contract allows you enter into a service agreement with
+                    another party. The person performing the service creates the
+                    contract. Once it’s deployed, the person receiving the service will
+                    add funds to the contract. Once the work is completed, both
+                    parties will finalize the contract, via your dashboards and funds
+                    will be released.
+                  </Typography>
+                <Button variant="contained" color="primary" className={classes.button} component={Link} to='/deploy/service_agreement'>
                   Create This Contract
-              </Button>
-            </CardContent>
-          </Card>
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
 
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography className={classes.heading}>
+                  Rainy Day Contract
+                </Typography>
+                <Typography className={classes.secondaryHeading} />
+                <Typography>
+                  This contract allows you to hold money for a rainy day in which
+                  ever city you specify. Once the weather report shows that it’s
+                  raining, the money is transferred into your account and you are
+                  free to enjoy your rainy day.
+                </Typography>
+                <Button variant="contained" color="primary" className={classes.button} component={Link} to='/deploywithvalue/rainy_day'>
+                    Create This Contract
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+
+</Grid>
         </div>
       </SideBar>
     );
